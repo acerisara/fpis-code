5a7d3ebabfc3c0bf1566418dc3d8785987b4c113
Andrea Cerisara <andreacerisara@gmail.com>
2022-10-21 20:36:13 +1100

    Chapter 4 exercises

---
+++ app/src/main/scala/fpis/code/chapter4/Either.scala
@@ -0,0 +1,50 @@
+package fpis.code.chapter4
+
+trait Either[+E, +A] {
+
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(v) => Right(f(v))
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e)  => Left(e)
+      case Right(v) => f(v)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(_)  => b
+      case Right(_) => this
+    }
+
+  def map2[EE >: E, B, C](eb: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      b <- eb
+    } yield f(a, b)
+
+}
+
+object Either {
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    es.foldLeft(Right(List.empty[A]): Either[E, List[A]])((es, e) =>
+      es.map2(e)(_ :+ _)
+    )
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldLeft(Right(List.empty[B]): Either[E, List[B]])((es, a) =>
+      es.map2(f(a))(_ :+ _)
+    )
+
+  def sequenceT[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(e => e)
+
+}
+
+case class Left[+EE](value: EE) extends Either[EE, Nothing]
+
+case class Right[+AA](value: AA) extends Either[Nothing, AA]
