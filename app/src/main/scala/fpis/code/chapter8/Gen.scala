9fa13460b313ac28575a8000e54a3808983034f9
Andrea Cerisara <andreacerisara@gmail.com>
2022-12-07 20:33:39 +1100

    Chapter 8 exercises

--- app/src/main/scala/fpis/code/chapter8/Gen.scala
+++ app/src/main/scala/fpis/code/chapter8/Gen.scala
@@ -2,7 +2,15 @@ package fpis.code.chapter8
 
 import fpis.code.chapter6.{RNG, SimpleRNG, State}
 
-case class Gen[A](sample: State[RNG, A])
+case class Gen[A](sample: State[RNG, A]) {
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size.flatMap(n => Gen.listOfN(n, this))
+
+}
 
 object Gen {
 
@@ -24,5 +32,18 @@ object Gen {
     Gen(
       State.sequence(List.fill(n)(g.sample))
     )
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap(if (_) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+
+    Gen(
+      State(SimpleRNG.double).flatMap(d =>
+        if (d < threshold) g1._1.sample else g2._1.sample
+      )
+    )
+  }
 
 }
