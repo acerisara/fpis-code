1a3d321e42b4543eb729ce559f033e0abc9044bf
Andrea Cerisara <andreacerisara@gmail.com>
2022-12-09 07:45:22 +1100

    Chapter 8 exercises

--- app/src/main/scala/fpis/code/chapter8/Prop.scala
+++ app/src/main/scala/fpis/code/chapter8/Prop.scala
@@ -1,26 +1,48 @@
 package fpis.code.chapter8
 
-import fpis.code.chapter8.Prop.{FailedCase, SuccessCount}
+import fpis.code.chapter6.RNG
+import fpis.code.chapter8.Prop.{FailedCase, SuccessCount, TestCases}
 
-trait Prop {
+case class Prop(run: (TestCases, RNG) => Result)
 
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
-
-  def &&(p: Prop): Prop = new Prop {
-    override def check: Either[(FailedCase, SuccessCount), SuccessCount] =
-      (this.check, p.check) match {
-        case (Right(s1), Right(s2)) => Right(s1 + s2)
-        case (Right(s), Left(e))    => Left((e._1, e._2 + s))
-        case (Left(e), Right(s))    => Left((e._1, e._2 + s))
-        case (Left(e1), Left(e2))   => Left((e1._1, e1._2 + e2._2))
-      }
-  }
-
+sealed trait Result {
+  def isFalsified: Boolean
+}
+case object Passed extends Result {
+  def isFalsified = false
+}
+case class Falsified(failure: FailedCase, successes: SuccessCount)
+    extends Result {
+  def isFalsified = true
 }
 
 object Prop {
 
+  type TestCases = Int
   type FailedCase = String
   type SuccessCount = Int
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+    randomStream(as)(rng)
+      .zip(LazyList.from(0))
+      .take(n)
+      .map { case (a, i) =>
+        try {
+          if (f(a)) Passed else Falsified(a.toString, i)
+        } catch {
+          case e: Exception => Falsified(buildMsg(a, e), i)
+        }
+      }
+      .find(_.isFalsified)
+      .getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): LazyList[A] =
+    LazyList.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
 
 }
