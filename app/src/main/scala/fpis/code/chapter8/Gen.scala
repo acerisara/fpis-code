0ebf1aceb62f8e430f1318b36b07c7886bdbec86
Andrea Cerisara <andreacerisara@gmail.com>
2022-12-10 16:22:17 +1100

    Chapter 8 exercises

--- app/src/main/scala/fpis/code/chapter8/Gen.scala
+++ app/src/main/scala/fpis/code/chapter8/Gen.scala
@@ -2,13 +2,25 @@ package fpis.code.chapter8
 
 import fpis.code.chapter6.{RNG, SimpleRNG, State}
 
-case class Gen[A](sample: State[RNG, A]) {
+case class Gen[+A](sample: State[RNG, A]) {
 
   def flatMap[B](f: A => Gen[B]): Gen[B] =
     Gen(sample.flatMap(a => f(a).sample))
 
   def listOfN(size: Gen[Int]): Gen[List[A]] =
     size.flatMap(n => Gen.listOfN(n, this))
+
+  def unsized: SGen[A] = SGen(_ => this)
+
+}
+
+case class SGen[+A](forSize: Int => Gen[A]) {
+
+  def flatMap[B](f: A => SGen[B]): SGen[B] =
+    SGen(n => forSize(n).flatMap(a => f(a).forSize(n)))
+
+  def listOf[B](g: Gen[B]): SGen[List[B]] =
+    SGen(n => Gen.listOfN(n, g))
 
 }
 
@@ -24,14 +36,10 @@ object Gen {
   def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
 
   def boolean: Gen[Boolean] =
-    Gen(
-      State(SimpleRNG.nonNegativeInt).map(n => n % 2 == 0)
-    )
+    Gen(State(SimpleRNG.nonNegativeInt).map(n => n % 2 == 0))
 
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
-    Gen(
-      State.sequence(List.fill(n)(g.sample))
-    )
+    Gen(State.sequence(List.fill(n)(g.sample)))
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
     boolean.flatMap(if (_) g1 else g2)
--- app/src/main/scala/fpis/code/chapter8/Prop.scala
+++ app/src/main/scala/fpis/code/chapter8/Prop.scala
@@ -1,16 +1,34 @@
 package fpis.code.chapter8
 
 import fpis.code.chapter6.RNG
-import fpis.code.chapter8.Prop.{FailedCase, SuccessCount, TestCases}
+import fpis.code.chapter8.Prop.{FailedCase, MaxSize, SuccessCount, TestCases}
 
-case class Prop(run: (TestCases, RNG) => Result)
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+
+  def &&(p: Prop): Prop = Prop { (m, n, rng) =>
+    run(m, n, rng) match {
+      case Passed => p.run(m, n, rng)
+      case x      => x
+    }
+  }
+
+  def ||(p: Prop): Prop = Prop { (m, n, rng) =>
+    run(m, n, rng) match {
+      case Falsified(_, _) => p.run(m, n, rng)
+      case x               => x
+    }
+  }
+
+}
 
 sealed trait Result {
   def isFalsified: Boolean
 }
+
 case object Passed extends Result {
   def isFalsified = false
 }
+
 case class Falsified(failure: FailedCase, successes: SuccessCount)
     extends Result {
   def isFalsified = true
@@ -21,8 +39,9 @@ object Prop {
   type TestCases = Int
   type FailedCase = String
   type SuccessCount = Int
+  type MaxSize = Int
 
-  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (_, n, rng) =>
     randomStream(as)(rng)
       .zip(LazyList.from(0))
       .take(n)
@@ -35,6 +54,26 @@ object Prop {
       }
       .find(_.isFalsified)
       .getOrElse(Passed)
+  }
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) =>
+      val casesPerSize = (n + (max - 1)) / max
+      val props: LazyList[Prop] =
+        LazyList.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+
+      val prop: Prop =
+        props
+          .map(p =>
+            Prop { (max, _, rng) =>
+              p.run(max, casesPerSize, rng)
+
+            }
+          )
+          .toList
+          .reduce(_ && _)
+
+      prop.run(max, n, rng)
   }
 
   def randomStream[A](g: Gen[A])(rng: RNG): LazyList[A] =
