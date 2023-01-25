b3911fd709471822d1437a2630d0cfe604d25073
Andrea Cerisara <andreacerisara@gmail.com>
2023-01-26 09:25:51 +1100

    Chapter 9 implementation

---
+++ app/src/main/scala/fpis/code/chapter9/Parsers.scala
@@ -0,0 +1,24 @@
+package fpis.code.chapter9
+
+import scala.language.implicitConversions
+
+trait Parsers[ParseError, Parser[+_]] { self =>
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def char(c: Char): Parser[Char]
+  def or[A](p: Parser[A], p2: Parser[A]): Parser[A]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit
+      f: A => Parser[String]
+  ): ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+  }
+
+}
