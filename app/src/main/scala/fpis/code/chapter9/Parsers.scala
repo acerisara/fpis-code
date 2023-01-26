fb196659d2697d04356f8282aea2e578953952c7
Andrea Cerisara <andreacerisara@gmail.com>
2023-01-27 06:59:43 +1100

    Chapter 9 implementation

--- app/src/main/scala/fpis/code/chapter9/Parsers.scala
+++ app/src/main/scala/fpis/code/chapter9/Parsers.scala
@@ -1,14 +1,19 @@
 package fpis.code.chapter9
 
+import fpis.code.chapter8.Prop.forAll
+import fpis.code.chapter8.{Gen, Prop}
+
 import scala.language.implicitConversions
 
 trait Parsers[ParseError, Parser[+_]] { self =>
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def char(c: Char): Parser[Char]
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
   def or[A](p: Parser[A], p2: Parser[A]): Parser[A]
-
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
@@ -19,6 +24,18 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def many(): Parser[List[A]] = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
   }
+
+  object Laws {
+    def equal[A](p: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
+  }
+
+  // TODO: Add law for succeed
 
 }
