6ac508c1eba129bc010d641855156ee11ebeb90c
Andrea Cerisara <andreacerisara@gmail.com>
2023-02-04 07:28:01 +1100

    Chapter 9 exercises

---
+++ app/src/main/scala/fpis/code/chapter9/JSON.scala
@@ -0,0 +1,12 @@
+package fpis.code.chapter9
+
+trait JSON
+
+object JSON {
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+}
--- app/src/main/scala/fpis/code/chapter9/Parsers.scala
+++ app/src/main/scala/fpis/code/chapter9/Parsers.scala
@@ -22,16 +22,25 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _)
 
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = flatMap(p) { a =>
+    succeed(f(a))
+  }
 
-  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+  def succeed[A](a: A): Parser[A]
 
   def slice[A](p: Parser[A]): Parser[String]
 
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    for {
+      a <- p
+      b <- p2
+    } yield (a, b)
 
   def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-    map(product(p, p2))(f.tupled)
+    for {
+      a <- p
+      b <- p2
+    } yield f(a, b)
 
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
