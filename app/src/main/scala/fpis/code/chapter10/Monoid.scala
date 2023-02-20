740ce462ba4954fe24d68c19a679983301579436
Andrea Cerisara <andreacerisara@gmail.com>
2023-02-20 19:07:07 +1100

    Chapter 10 exercises

---
+++ app/src/main/scala/fpis/code/chapter10/Monoid.scala
@@ -0,0 +1,48 @@
+package fpis.code.chapter10
+
+trait Monoid[A] {
+
+  def op(a1: A, a2: A): A
+
+  def zero: A
+
+}
+
+object Monoid {
+
+  val stringMonoid: Monoid[String] = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+    override def zero: String = ""
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+    override def zero: List[A] = Nil
+  }
+
+  val intAdditionMonoid: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+    override def zero: Int = 0
+  }
+
+  val intMultiplicationMonoid: Monoid[Int] = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+    override def zero: Int = 1
+  }
+
+  val booleanOrMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    override def zero: Boolean = false
+  }
+
+  val booleanAndMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    override def zero: Boolean = true
+  }
+
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = a1.orElse(a2)
+    override def zero: Option[A] = None
+  }
+
+}
