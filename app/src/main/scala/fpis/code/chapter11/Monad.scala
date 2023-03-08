4484a3fa4b703873b8625a70ed1c6fbf30c107a4
Andrea Cerisara <andreacerisara@gmail.com>
2023-03-08 20:03:58 +1100

    Chapter 11 exercises

---
+++ app/src/main/scala/fpis/code/chapter11/Monad.scala
@@ -0,0 +1,52 @@
+package fpis.code.chapter11
+
+import fpis.code.chapter7.Par
+import fpis.code.chapter7.Par.Par
+import fpis.code.chapter8.Gen
+import fpis.code.chapter9.MyParser.Parser
+import fpis.code.chapter9.Parsers
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  override def map[A, B](ma: F[A])(f: A => B): F[B] =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a, b)))
+
+}
+
+object Monad {
+
+  val genMonad: Monad[Gen] = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+    def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] = fa.flatMap(f)
+  }
+
+  val parMonad: Monad[Par] = new Monad[Par] {
+    override def unit[A](a: => A): Par[A] = Par.unit(a)
+    override def flatMap[A, B](fa: Par[A])(f: A => Par[B]): Par[B] =
+      Par.flatMap(fa)(f)
+  }
+
+  def parserMonad(p: Parsers[Parser]): Monad[Parser] = new Monad[Parser] {
+    override def unit[A](a: => A): Parser[A] = p.succeed(a)
+    override def flatMap[A, B](fa: Parser[A])(f: A => Parser[B]): Parser[B] =
+      p.flatMap(fa)(f)
+  }
+
+  val optionMonad: Monad[Option] = new Monad[Option] {
+    override def unit[A](a: => A): Option[A] = Some(a)
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] =
+      fa.flatMap(f)
+  }
+
+  val listMonad: Monad[List] = new Monad[List] {
+    override def unit[A](a: => A): List[A] = List(a)
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] =
+      fa.flatMap(f)
+  }
+
+}
