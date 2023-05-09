391c2199ad2880fb094eb815ae5322f2a3b05dc1
Andrea Cerisara <acerisara@seek.com.au>
2023-05-10 09:04:35 +1000

    Chapter 14 exercises

---
+++ app/src/test/scala/fpis/code/chapter14/STTest.scala
@@ -0,0 +1,29 @@
+package fpis.code.chapter14
+
+import org.junit.runner.RunWith
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.be
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class STTest extends AnyFunSuite {
+
+  test("simple program to swap ints") {
+    val p = new RunnableST[(Int, Int)] {
+      def apply[S]: ST[S, (Int, Int)] = for {
+        r1 <- STRef(1)
+        r2 <- STRef(2)
+        x <- r1.read
+        y <- r2.read
+        _ <- r1.write(y)
+        _ <- r2.write(x)
+        a <- r1.read
+        b <- r2.read
+      } yield (a, b)
+    }
+
+    ST.runST(p) should be((2, 1))
+  }
+
+}
